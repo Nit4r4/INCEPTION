c9b20735b949c56b136429f5d0cffdca7fbab17e
Nit4r4 <87270463+Nit4r4@users.noreply.github.com>
2023-09-21 15:42:09 +0200

    tourne en rond entre pbl socket, password et connection. Probablement un soucis avec mariadb.sh et le Dockerfile

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -5,13 +5,17 @@
 #l.12 on change le mot de passe et les droits root pour limiter l'acces a l'admin
 #l.13 rafraichir tout cela pour que MySQL le prenne en compte
 
-cat << EOF > /tmp/init_db.sql
+# cat << EOF < /tmp/init_db.sql
+echo 
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('rootPwd');
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
-EOF
+>> /tmp/init_db.sql
 
 
 # mysql -h localhost < /tmp/init_db.sql # on demarre mysql dans le Dockerfile end demarrant le script
+#Autoriser les connexions depuis n'importe quel hôte : Lorsque vous attribuez des autorisations à l'utilisateur MariaDB, 
+#utilisez '%' comme hôte au lieu d'une adresse IP spécifique. Cela autorisera les connexions depuis n'importe quel hôte.
